31d38fa72a979135de4ead499944c5e58eb394bc
Timothe <timothe.champieux@ynov.com>
2026-03-16 10:27:20 +0100

    feat: v2 de la fonction planet detector avec ajout du snr

--- src/analysis/detection.py
+++ src/analysis/detection.py
@@ -10,20 +10,35 @@ def _get_search_params(lc : lk.LightCurve) :
     min_period = 0.5
     steps = int(observation_time * 500)
     if max_period < min_period :
-        return observation_time,max_period+=min_period,steps,min_period 
-    return observation_time,max_period,steps,min_period 
+        return max_period+=min_period,steps,min_period 
+    return max_period,steps,min_period 
     
 
 def planet_detector(lc : lk.LightCurve) : 
-    obs_time, max_p, steps,min_p  = _get_search_params(lc)
+
+    max_p, steps,min_p  = _get_search_params(lc)
+
     periods =  np.linspace(min_p,max_p,steps)
     bls = lc.to_periodogram(method='bls',period=periods)
+    
+    best_period = bls.period_at_max_power
+    best_t0 = bls.transit_time_at_max_power
+    best_duration = bls.duration_at_max_power
+
+    stats = bls.compute_stats(period=best_period, 
+                              duration=best_duration, 
+                              transit_time=best_t0)
+
+    snr = stats['snr']
+
     result = {
-    "period_at_max_power": bls.period_at_max_power,
-    "transit_time_at_max_power": bls.transit_time_at_max_power,
-    "duration_at_max_power": bls.duration_at_max_power,
-    "max_power" : bls.max_power 
+        "period": best_period.value,
+        "transit_time": best_t0.value,
+        "duration": best_duration.value,
+        "snr": snr,
+        "max_power": bls.max_power.value
     }
+    
     return result
 
 
